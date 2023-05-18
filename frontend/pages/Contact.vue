bb4ab07282123d79ff8a4523db68430b04081056
Martin Bullman <admin@quickminutes.com>
2023-05-19 00:53:42 +0200

    Frontend initial pages

---
+++ frontend/pages/Contact.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+
+    export default {
+        name: 'Contact'
+    }
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/pages/Projects.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+
+    export default {
+        name: 'Projects'
+    }
+
+</script>
+
+<style scoped>
+
+</style>
