0fd5208bca44b40f7a2867b85a083367e1d2cc93
Martin Bullman <admin@quickminutes.com>
2023-05-24 15:43:40 +0300

    Implemented new Vuetify plugin which includes custom themes

--- frontend/plugins/vuetify.ts
+++ frontend/plugins/vuetify.ts
@@ -1,14 +1,45 @@
-// plugins/vuetify.js
-import { createVuetify } from 'vuetify';
-import * as components   from 'vuetify/components';
-import * as directives   from 'vuetify/directives';
+// vuetify plugin.
+import { createVuetify, ThemeDefinition } from 'vuetify';
+
+const customLightTheme: ThemeDefinition = {
+    dark: false,
+    colors: {
+        primary              : '#2196F3',
+        'primary-darken-1'   : '#1E88E5',
+        secondary            : '#FF5722',
+        'secondary-darken-1' : '#F4511E',
+        error                : '#F44336',
+        info                 : '#2196F3',
+        success              : '#4CAF50',
+        warning              : '#FF9800'
+    }
+}
+
+const customDarkTheme: ThemeDefinition = {
+    dark: true,
+    colors: {
+        primary              : '#FF5722',
+        'primary-darken-1'   : '#F4511E',
+        secondary            : '#2196F3',
+        'secondary-darken-1' : '#1E88E5',
+        error                : '#F44336',
+        info                 : '#2196F3',
+        success              : '#4CAF50',
+        warning              : '#FF9800'
+    }
+}
 
 export default defineNuxtPlugin(nuxtApp => {
     const vuetify = createVuetify({
         ssr: true,
-        components,
-        directives
-    })
+        theme: {
+            defaultTheme: 'customLightTheme',
+            themes: {
+                customDarkTheme,
+                customLightTheme
+            },
+          },
+    });
 
     nuxtApp.vueApp.use(vuetify);
-})
+});
