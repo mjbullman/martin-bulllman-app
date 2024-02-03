e988cb87062d18d1065e35508e19b2430337abde
Martin Bullman <admin@quickminutes.com>
2024-02-04 00:56:43 +0200

    Added app main

---
+++ frontend/components/layout/AppMain.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <v-main>
+
+        <!-- nuxt router navigation-->
+        <nuxt-page></nuxt-page>
+
+    </v-main>
+
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+    .v-main {
+        --v-layout-top: 64px !important;
+    }
+
+</style>
