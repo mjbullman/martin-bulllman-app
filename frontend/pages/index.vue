7c847192c3f59393d047d32a75acf0a5689957e9
Martin Bullman <admin@quickminutes.com>
2024-02-04 23:53:44 +0200

    Index view code clean up

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,5 @@
 <template>
 
-    <div>
-
     <v-container class="fill-height">
 
         <v-row class="d-flex flex-column flex-sm-row align-content-space-between flex-al mt-5 mt-md-0" justify="center" align="center">
@@ -32,39 +30,6 @@
 
     </v-container>
 
-    <v-container>
-
-        <v-timeline align="start">
-    <v-timeline-item
-      v-for="(year, i) in years"
-      :key="i"
-      :dot-color="year.color"
-      size="small"
-    >
-      <template v-slot:opposite>
-        <div
-          :class="`pt-1 headline font-weight-bold text-${year.color}`"
-          v-text="year.year"
-        ></div>
-      </template>
-      <div>
-        <h2 :class="`mt-n1 headline font-weight-light mb-4 text-${year.color}`">
-          Lorem ipsum
-        </h2>
-         <v-card class="elevation-2">
-        <v-card-title class="text-h5">
-          Lorem ipsum
-        </v-card-title>
-        <v-card-text>Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.</v-card-text>
-      </v-card>
-      </div>
-    </v-timeline-item>
-  </v-timeline>
-
-    </v-container>
-
-    </div>
-
 </template>
 
 <script setup lang="ts">
@@ -74,29 +39,6 @@
 
     // state.
     const settings = useSettingsStore();
-
-     const years = [
-        {
-          color: 'cyan',
-          year: '1960',
-        },
-        {
-          color: 'green',
-          year: '1970',
-        },
-        {
-          color: 'pink',
-          year: '1980',
-        },
-        {
-          color: 'amber',
-          year: '1990',
-        },
-        {
-          color: 'orange',
-          year: '2000',
-        },
-      ]
 
 </script>
 
