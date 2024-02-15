c470dd941922f3715e46e432f7b5f4f8b2a74c56
Martin Bullman <admin@quickminutes.com>
2024-02-15 12:01:50 +0200

    Implemented new URL and API method to get the current weather forecast

--- backend/martinbullman/core/urls.py
+++ backend/martinbullman/core/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test', views.MartinbullmanAPIView.as_view())
+    path('test', views.MartinbullmanAPIView.as_view()),
+    path('weather', views.WeatherForecastData.as_view())
 ]
--- backend/martinbullman/core/views.py
+++ backend/martinbullman/core/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render
+import requests
+from decouple import config
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.throttling import UserRateThrottle
+
 
 class MartinbullmanAPIView(APIView):
     def get(self, request):
         return Response('Hello World!')
 
 
-class WeatherForecastAPIView(APIView):
+class WeatherForecastData(APIView):
+    """
+    Gets the current weather forcast data from the weather API.
+    """
+    throttle_classes = [UserRateThrottle]
+
     def get(self, request):
+        response = requests.get(config('WEATHER_API_URL') + '/current.json', {
+            'key': config('WEATHER_API_KEY'),
+            'q': 'Tallinn'})
 
-
-        return Response('Hello World!')
-
+        return Response(response.json())
