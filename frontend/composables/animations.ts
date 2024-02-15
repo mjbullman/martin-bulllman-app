6af41afaa0adf319554705e48d0b8e2e22fae160
Martin Bullman <admin@quickminutes.com>
2024-02-15 13:20:09 +0200

    Fade up animation

--- frontend/composables/animations.ts
+++ frontend/composables/animations.ts
@@ -2,21 +2,19 @@ import { ref } from 'vue'
 
 export function useAnimations () {
 
-    function fadeUp () {
-        return ref({
-            initial: {
-                opacity: 0,
-                y: 250
-            },
-            enter: {
-                opacity: 1,
-                y: 0,
-                transition: {
-                    duration: 500
-                }
+    const fadeUp = ref({
+        initial: {
+            opacity: 0,
+            y: 250
+        },
+        enter: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: 500
             }
-        })
-    }
+        }
+    })
 
     return { fadeUp }
 }
