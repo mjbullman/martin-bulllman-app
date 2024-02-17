b796d8ed6145e3913486c797e0411da97565ea89
Martin Bullman <admin@quickminutes.com>
2024-02-17 10:13:40 +0200

    Page Header Component

---
+++ frontend/components/general/PageHeading.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <v-container>
+
+            <v-row>
+
+                <v-col cols="12" sm="12" md="12" lg="12" xl="12" align-self="center" class="pt-5 pb-5 pt-md-12 pb-md-12">
+
+                    <h1 class="text-h2 text-md-h1 pb-5 text-center font-weight-bold text-gradient-animation">
+
+                        {{ props.headingText }}
+
+                    </h1>
+
+                </v-col>
+
+            </v-row>
+
+        </v-container>
+
+</template>
+
+<script setup lang="ts">
+
+    const props = defineProps<{
+        headingText?: String
+    }>()
+
+</script>
+
+<style scoped>
+
+</style>
