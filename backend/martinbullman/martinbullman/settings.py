7cd08dd5694fcc1b990b4be8ba1ab88da6d24c3a
Martin Bullman <martin.j.bullman@gmail.com>
2024-02-20 01:22:31 +0200

    Adding the Spotify & Weather apps

--- backend/martinbullman/martinbullman/settings.py
+++ backend/martinbullman/martinbullman/settings.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'core',
+    'spotify',
+    'weather'
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -64,10 +66,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'martinbullman.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -79,10 +79,8 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -98,7 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -110,7 +107,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
@@ -118,15 +114,9 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    #'DEFAULT_PERMISSION_CLASSES': [
-    #    'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    #],
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.AnonRateThrottle',
         'rest_framework.throttling.UserRateThrottle'
--- backend/martinbullman/martinbullman/urls.py
+++ backend/martinbullman/martinbullman/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('core.urls'))
+    path('api/v1/', include('core.urls')),
+    path('api/v1/spotify/', include('spotify.urls')),
+    path('api/v1/weather/', include('weather.urls'))
 ]
