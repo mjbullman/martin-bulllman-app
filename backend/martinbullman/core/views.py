89dc20e733e993d2e0d23d03f992a0275d0538c8
Martin Bullman <martin.j.bullman@gmail.com>
2024-02-21 23:59:04 +0200

    Contact form api method

--- backend/martinbullman/core/views.py
+++ backend/martinbullman/core/views.py
@@ -1,11 +1,26 @@
-import requests
-from decouple import config
-from rest_framework.response import Response
+from django.core.mail import send_mail
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.throttling import UserRateThrottle
+
+
+class ContactForm(APIView):
+    """ Handle the contact form submission and send email to me. """
+    throttle_classes = [UserRateThrottle]
+
+    def post (self, request):
+        send_mail(
+            subject        = 'New Message from martinbullman.xyz',
+            message        = 'That’s your message body',
+            from_email     = 'martin.j.bullman@gmail.com',
+            fail_silently  = False,
+            recipient_list = ['admin@quickminutes.com'],
+        )
+
+        return Response(request.data['name'])
 
 
 class MartinbullmanAPIView(APIView):
 
-    def get(self, request):
+    def get (self, request):
         return Response('Hello World!')
