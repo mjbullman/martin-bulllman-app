649cc381389da9b458d5783deeb18fb67190f3b8
Martin Bullman <martin.j.bullman@gmail.com>
2024-02-27 12:24:45 +0200

    Weather app url updates

--- backend/martinbullman/weather/urls.py
+++ backend/martinbullman/weather/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('current', views.WeatherForecastData.as_view()),
-    path('forecast', views.WeatherForecastData.as_view())
+    path('current', views.WeatherCurrent.as_view()),
+    path('forecast', views.WeatherForecast.as_view())
 ]
