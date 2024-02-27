301610bdaa762c575c3fba4e78691444e9ad07ba
Martin Bullman <martin.j.bullman@gmail.com>
2024-02-27 12:22:13 +0200

    Updates to the weather app api implementation

--- backend/martinbullman/weather/views.py
+++ backend/martinbullman/weather/views.py
@@ -1,20 +1,45 @@
 import requests
 from decouple import config
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.throttling import UserRateThrottle
 
+# weather api url
+weather_api_url = 'https://api.weatherapi.com/v1'
 
-class WeatherForecastData(APIView):
+
+class WeatherCurrent(APIView):
     """
-    Gets the current weather forcast data from the weather API.
+    Gets the current weather data from the weather API.
     """
     throttle_classes = [UserRateThrottle]
 
     def get(self, request):
 
-        response = requests.get(config('WEATHER_API_URL') + '/current.json', {
+        response = requests.get(weather_api_url + '/current.json', {
             'key': config('WEATHER_API_KEY'),
-            'q': 'Tallinn'})
+            'q'  : 'Tallinn',
+            'aqi': 'yes'
+        })
+
+        return Response(response.json())
+
+
+class WeatherForecast(APIView):
+    """
+    Gets the ten-day weather forcast data from the weather API.
+    """
+    throttle_classes = [UserRateThrottle]
+
+    def get(self, request):
+
+        response = requests.get(weather_api_url + '/forecast.json', {
+            'key'    : config('WEATHER_API_KEY'),
+            'q'      : 'Tallinn',
+            'aqi'    : 'yes',
+            'days'   : 10,
+            'alerts' : 'yes'
+        })
 
         return Response(response.json())
