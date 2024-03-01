e10f85473778f1b51dbb084057d38747efcbd0f6
Martin Bullman <martin.j.bullman@gmail.com>
2024-03-01 14:06:20 +0200

    Spotify tests updates

--- backend/martinbullman/spotify/tests.py
+++ backend/martinbullman/spotify/tests.py
@@ -11,6 +11,19 @@ class SpotifyTest (APITestCase):
         """ Set up API client used for testing the spotify app """
         self.client = APIClient()
 
+    def test_spotify_profile (self):
+        """ Test the get profile API endpoint """
+        response = self.client.get(spotify_api_url + '/profile', format='json')
+
+        # successful responses.
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # contains profile display name.
+        self.assertIn('display_name', response.data)
+
+        # contains profile email.
+        self.assertIn('email', response.data)
+
     def test_spotify_playlist (self):
         """ Test the get playlist API endpoint """
         response = self.client.get(spotify_api_url + '/playlists', format='json')
@@ -24,6 +37,19 @@ class SpotifyTest (APITestCase):
         # contains total items.
         self.assertIn('total', response.data)
 
+    def test_spotify_following (self):
+        """ Test the following artists API endpoint """
+        response = self.client.get(spotify_api_url + '/following', format='json')
+
+        # successful responses.
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # contains artists items.
+        self.assertIn('artists', response.data)
+
+        # contains following items.
+        self.assertIn('items', response.data)
+
     def test_spotify_top_tracks (self):
         """ Test the get top tracks API endpoint """
         response = self.client.get(spotify_api_url + '/top_tracks', format='json')
@@ -31,7 +57,20 @@ class SpotifyTest (APITestCase):
         # successful responses.
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        # contains playlist items.
+        # contains top tracks items.
+        self.assertIn('items', response.data)
+
+        # contains total items.
+        self.assertIn('total', response.data)
+
+    def test_spotify_top_artists (self):
+        """ Test the get top artists API endpoint """
+        response = self.client.get(spotify_api_url + '/top_artists', format='json')
+
+        # successful responses.
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # contains top artists items.
         self.assertIn('items', response.data)
 
         # contains total items.
