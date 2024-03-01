810676e551c516f26d74610e66b771acfeb4e9d6
Martin Bullman <martin.j.bullman@gmail.com>
2024-03-01 13:53:15 +0200

    Initial resume component

---
+++ frontend/components/buttons/Resume.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+    <v-btn  variant="outlined" color="primary">
+
+        Resume
+
+    </v-btn>
+
+</template>
+
+<style scoped>
+
+</style>
