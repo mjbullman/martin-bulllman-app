51f0b5529020d1bd7285deea7be195f176637d5a
Martin Bullman <martin.j.bullman@gmail.com>
2024-03-01 13:55:11 +0200

    Contact form backend implementation

--- backend/martinbullman/core/views.py
+++ backend/martinbullman/core/views.py
@@ -1,3 +1,6 @@
+import requests
+from decouple import config
+
 from django.core.mail import send_mail
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -9,15 +12,31 @@ class ContactForm(APIView):
     throttle_classes = [UserRateThrottle]
 
     def post (self, request):
-        send_mail(
-            subject        = 'New Message from martinbullman.xyz',
-            message        = 'That’s your message body',
-            from_email     = 'martin.j.bullman@gmail.com',
-            fail_silently  = False,
-            recipient_list = ['admin@quickminutes.com'],
-        )
+        name    = request.data.get('name')
+        email   = request.data.get('email')
+        token   = request.data.get('recaptcha_token')
+        message = request.data.get('message')
 
-        return Response(request.data['name'])
+        recaptcha_data = {
+            'secret': config('GOOGLE_RECAPTCHA_SECRET_KEY'),
+            'response': token
+        }
+
+        response = requests.post('https://www.google.com/recaptcha/api/siteverify', data=recaptcha_data)
+
+        result = response.json()
+        if result.get('success') and result.get('score') > 0.5:
+            send_mail(
+                subject='New Message from: ' + name,
+                message=message,
+                from_email=email,
+                fail_silently=False,
+                recipient_list=['martin.j.bullman@gmail.com'],
+            )
+
+            return Response('Thank you for reaching out! I\'ll get back to you as soon as possible.', 200)
+        else:
+            return Response('Thank you for reaching out! I\'ll get back to you as soon as possible.', 500)
 
 
 class MartinbullmanAPIView(APIView):
