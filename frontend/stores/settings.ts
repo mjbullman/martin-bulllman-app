969acbb838dc344ed8ce25a40a3b42303f0f658f
Martin Bullman <martin.j.bullman@gmail.com>
2024-03-01 15:25:28 +0200

    Dark mode bug fix

--- frontend/stores/settings.ts
+++ frontend/stores/settings.ts
@@ -1,22 +1,24 @@
-import { useTheme }    from 'vuetify';
-import { defineStore } from 'pinia';
+import { useTheme }    from 'vuetify'
+import { defineStore } from 'pinia'
 
 export const useSettingsStore = defineStore('settings', () => {
-    const theme = useTheme();
+    const theme = useTheme()
+
+    theme.global.name.value = 'customDarkTheme'
 
     // state.
     const darkMode = ref<boolean>(true);
-    const cookie   = useCookie<boolean>('dark-mode', { default: () => (darkMode.value), watch: true });
+    const cookie   = useCookie<boolean>('dark-mode', { default: () => (darkMode.value), watch: true })
 
     // getters.
-    const getDarkMode = computed(() => darkMode.value);
+    const getDarkMode = computed(() => darkMode.value)
 
     // actions.
     function setDarkMode () : void {
         // update the dark mode state value.
-        darkMode.value          = !darkMode.value;
+        darkMode.value          = !darkMode.value
         // update the dark mode cookie value.
-        cookie.value            = darkMode.value;
+        cookie.value            = darkMode.value
         // update the dark mode vuetify theme.
         theme.global.name.value = darkMode.value ? 'customDarkTheme' : 'customLightTheme';
     }
