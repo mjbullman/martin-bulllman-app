70c275730b991c9f84f4924a96c7a3dd98ca3555
Martin Bullman <martin.j.bullman@gmail.com>
2024-03-01 13:57:50 +0200

    Spotify API urls updates

--- backend/martinbullman/spotify/urls.py
+++ backend/martinbullman/spotify/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('profile', views.Profile().as_view()),
     path('playlists', views.Playlist().as_view()),
+    path('following', views.Following().as_view()),
     path('top_tracks', views.TopTracks().as_view()),
+    path('top_artists', views.TopArtists().as_view()),
     path('recently_played', views.RecentlyPlayed().as_view()),
     path('currently_playing', views.CurrentlyPlaying().as_view())
 ]
