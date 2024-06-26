bd52d81faa3c53459543245e5765f87e5c9bf029
Martin Bullman <martin.j.bullman@gmail.com>
2024-06-26 23:12:51 +0300

    Added new icons

--- frontend/plugins/fontawesome.ts
+++ frontend/plugins/fontawesome.ts
@@ -1,22 +1,22 @@
 // fontawesome plugin.
-import { library, config } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faGlobe, faBars, faX, faXmarkCircle, faS }
-                           from '@fortawesome/free-solid-svg-icons';
-import { faMoon, faSun, faEnvelope, faCircleXmark, faMessage,  faUser }
-                           from '@fortawesome/free-regular-svg-icons';
-import { faLinkedinIn,faGithubAlt, faInstagram, faFacebook, faXTwitter, faYoutube }
-                           from '@fortawesome/free-brands-svg-icons';
+import { library, config } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faGlobe, faBars, faX, faXmarkCircle, faArrowUpRightFromSquare, faLink }
+                           from '@fortawesome/free-solid-svg-icons'
+import { faMoon, faSun, faEnvelope, faCircleXmark, faMessage,  faUser, faFilePdf}
+                           from '@fortawesome/free-regular-svg-icons'
+import { faLinkedinIn,faGithubAlt, faInstagram, faFacebook, faXTwitter, faYoutube, faGithub }
+                           from '@fortawesome/free-brands-svg-icons'
 
 library.add(
-    faLinkedinIn, faGithubAlt, faInstagram, faFacebook, faXTwitter,
+    faLinkedinIn, faGithubAlt, faInstagram, faFacebook, faXTwitter, faGithub,
     faBars, faGlobe, faSun, faMoon, faEnvelope, faX, faCircleXmark, faYoutube,
-    faXmarkCircle, faMessage, faUser
+    faXmarkCircle, faMessage, faUser, faFilePdf, faArrowUpRightFromSquare, faLink
 );
 
 // this is important, we are going to let Nuxt worry about the CSS
 config.autoAddCss = false;
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon);
+    nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon)
 });
