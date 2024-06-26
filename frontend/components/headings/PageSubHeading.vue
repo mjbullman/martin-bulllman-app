4119125911881922c7aeb4dd0bbc64025cde4f41
Martin Bullman <martin.j.bullman@gmail.com>
2024-06-26 23:16:35 +0300

    Section heading components

---
+++ frontend/components/headings/PageSubHeading.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <v-container>
+
+        <v-row>
+
+            <v-col cols="12" sm="12" md="12" lg="12" xl="12" align-self="center" class="">
+
+                <h1 class="text-h3 pb-5 text-center text-gradient-animation">
+
+                    {{ props.subHeadingText }}
+
+                </h1>
+
+            </v-col>
+
+        </v-row>
+
+    </v-container>
+
+</template>
+
+<script setup lang="ts">
+
+    const props = defineProps<{
+        subHeadingText?: String
+    }>()
+
+</script>
+
+<style scoped>
+
+</style>
