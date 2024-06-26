da436ba4ca1af441f40b6028010aa21e86c86d23
Martin Bullman <martin.j.bullman@gmail.com>
2024-06-26 23:15:05 +0300

    Class renaming

--- frontend/components/layout/PageHeading.vue
+++ frontend/components/layout/PageHeading.vue
@@ -6,7 +6,7 @@
 
                 <v-col cols="12" sm="12" md="12" lg="12" xl="12" align-self="center" class="pt-5 pb-5 pt-md-12 pb-md-12">
 
-                    <h1 class="text-h2 text-md-h1 pb-5 text-center font-weight-bold text-gradient-animation">
+                    <h1 class="text-h3 text-gradient-animation text-md-h1 pb-5 text-center font-weight-bold">
 
                         {{ props.headingText }}
 
