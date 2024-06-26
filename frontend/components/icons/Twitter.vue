793d06d678e8717876f72f1da90fe56d8546b2fd
Martin Bullman <martin.j.bullman@gmail.com>
2024-06-26 23:14:16 +0300

    Class renaming for social icons

--- frontend/components/icons/Twitter.vue
+++ frontend/components/icons/Twitter.vue
@@ -4,7 +4,7 @@
         variant="text" icon :size="size"
         href="https://twitter.com/mjbullman"
         target="_blank"
-        class="social-link"
+        class="slide-up"
         aria-label="View my X profile">
 
         <svg xmlns="http://www.w3.org/2000/svg" :width="width" :height="height" viewBox="0 0 24 24" fill="none" :stroke="color" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
