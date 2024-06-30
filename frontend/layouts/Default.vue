0c24d1436266aa16d49e3c6e3ec4d0fc65edab2b
Martin Bullman <martin.j.bullman@gmail.com>
2024-06-30 23:58:35 +0300

    Updated the layout component with the new footer component

--- frontend/layouts/Default.vue
+++ frontend/layouts/Default.vue
@@ -9,6 +9,9 @@
         <!-- application routes render -->
         <app-main></app-main>
 
+        <!-- application footer -->
+        <app-footer></app-footer>
+
         <!-- snack notifications -->
         <snack-bar></snack-bar>
 
@@ -20,6 +23,7 @@
 
     import AppMain          from '~/components/layout/AppMain.vue'
     import SnackBar         from '~/components/snackbars/SnackBar.vue'
+    import AppFooter        from '~/components/layout/AppFooter.vue'
     import AppNavigationBar from '~/components/layout/AppNavigationBar.vue'
 
 </script>
