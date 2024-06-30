1be62692091c2836b09c2d69915c89b6bc5b11af
Martin Bullman <martin.j.bullman@gmail.com>
2024-06-30 23:49:08 +0300

    Main page intro section

---
+++ frontend/components/sections/Intro.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <v-row class="text-center full-height" align="center" justify="center">
+
+        <v-col>
+
+            <h1 class="text-h2 text-md-h1 ma-5" >
+
+               <span class="text-gradient-animation">Martin Bullman</span>
+
+            </h1>
+
+            <h2 class="text-h6 text-md-h4 text-medium-emphasis ma-5" >
+
+                Software Engineer & Full Stack Developer
+
+            </h2>
+
+            <div class="text-body-1 ma-5">
+
+                Crafting innovative solutions for a digital world
+
+            </div>
+
+            <div>
+
+                <music-waves></music-waves>
+
+            </div>
+
+        </v-col>
+
+    </v-row>
+
+</template>
+
+<script setup lang="ts">
+
+    import MusicWaves from '~/components/music/MusicWaves.vue'
+
+</script>
+
+<style scoped>
+
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -3,37 +3,7 @@
     <v-container>
 
         <!-- intro -->
-        <v-row class="text-center full-height" align="center" justify="center">
-
-            <v-col>
-
-                <h1 class="text-h2 text-md-h1 ma-5" >
-
-                   <span class="text-gradient-animation">Martin Bullman</span>
-
-                </h1>
-
-                <h2 class="text-h6 text-md-h4 text-medium-emphasis ma-5" >
-
-                    Software Engineer & Full Stack Developer
-
-                </h2>
-
-                <div class="text-body-1 ma-5">
-
-                    Crafting innovative solutions for a digital world
-
-                </div>
-
-                <div>
-
-                    <music-waves></music-waves>
-
-                </div>
-
-            </v-col>
-
-        </v-row>
+        <intro></intro>
 
         <!-- projects -->
         <in-progress></in-progress>
@@ -50,8 +20,8 @@
 
 <script setup lang="ts">
 
+    import Intro        from '~/components/sections/Intro.vue'
     import Spotify      from '~/components/sections/Spotify.vue'
-    import MusicWaves   from '~/components/music/MusicWaves.vue'
     import InProgress   from '~/components/sections/InProgress.vue'
     import Publications from '~/components/sections/Publications.vue'
 
