ce7fb54ca89bf880d7d95f8d9702c8bfcb29065d
Martin Bullman <martin.j.bullman@gmail.com>
2024-07-02 08:16:07 +0300

    Tech icon base component

---
+++ frontend/components/icons/technologies/BaseIcon.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <div class="technology-icon-container text-center">
+
+        <svg viewBox="0 0 128 128" width="75" height="75" class="technology-icon-svg">
+
+            <slot></slot>
+
+        </svg>
+
+        <div v-if="label" class="technology-icon-text font-weight-bold">
+
+            {{ label }}
+
+        </div>
+
+    </div>
+
+</template>
+
+<script setup lang="ts">
+
+    defineProps({
+        label: { type: String, default: '' }
+    });
+
+</script>
+
+<style scoped>
+</style>
