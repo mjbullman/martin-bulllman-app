d9e5cb4e9f090d3694072f6ec587fdd583050df2
Martin Bullman <martin.j.bullman@gmail.com>
2024-08-10 23:56:38 +0300

    Update to the send email API endpoint

--- backend/martinbullman/core/views.py
+++ backend/martinbullman/core/views.py
@@ -1,8 +1,6 @@
 import requests
 from decouple import config
-
-from mailjet_rest import Client
-from django.core.mail import send_mail
+from utils.services.email import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.throttling import UserRateThrottle
@@ -29,18 +27,11 @@ class ContactForm(APIView):
         )
 
         result = response.json()
-        if result.get('success') and result.get('score') > 0.5:
 
-            
+        if result.get('success') and result.get('score') > float(config('GOOGLE_RECAPTCHA_SCORE')):
 
-
-            send_mail(
-                subject='New Message from: ' + name,
-                message=message,
-                from_email=email,
-                fail_silently=False,
-                recipient_list=['martin.j.bullman@gmail.com'],
-            )
+            receipt_result = send_contact_form_receipt_email(name, email)
+            message_result = send_contact_form_message_email(name, email, message)
 
             return Response('Thank you for reaching out! I\'ll get back to you as soon as possible.', 200)
         else:
