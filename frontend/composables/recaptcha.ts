5c124933547173c691b1d8d1ab417d233b124c37
Martin Bullman <martin.j.bullman@gmail.com>
2024-11-04 21:49:49 +0200

    Add comments recaptcha.ts

--- frontend/composables/recaptcha.ts
+++ frontend/composables/recaptcha.ts
@@ -1,25 +1,55 @@
 import { useReCaptcha } from 'vue-recaptcha-v3'
 
+/**
+ * A class that defines various actions associated with Google recaptcha.
+ * this class enables the creation of predefined action instances, ensuring
+ * that the actions are consistently identified by their unique names.
+ *
+ * The class is designed to simplify the integration of reCAPTCHA in the
+ * application by providing a clear and manageable way to reference
+ * specific actions, such as 'contact'. each action can be easily
+ * accessed and utilized when executing reCAPTCHA tasks.
+ */
 export class RecaptchaAction {
-    public static readonly contact= new RecaptchaAction('contact')
+    /**
+     * A predefined static instance for the 'contact' action.
+     *
+     * @type {RecaptchaAction}
+     */
+    public static readonly contact = new RecaptchaAction('contact')
+
+    /**
+     * Creates an instance of RecaptchaAction.
+     *
+     * @param {string} name - The name of the reCAPTCHA action.
+     * @private
+     */
     private constructor(public readonly name: string) {}
 }
 
 /**
- * The exported executeRecaptcha function allows you to execute reCAPTCHA actions and retrieve the reCAPTCHA
- * token along with the header options to be used in subsequent requests.
+ * A composable function that provides reCAPTCHA functionalities.
+ *
+ * @returns {Object} An object containing the `executeRecaptcha` function.
+ * @returns {Function} return.executeRecaptcha - A function to execute a reCAPTCHA action and retrieve the token and header options.
  */
 export default () => {
-    let recaptchaInstance= useReCaptcha()
+    // initialize the reCAPTCHA instance using the composable from vue-recaptcha-v3.
+    let recaptchaInstance = useReCaptcha()
 
+    /**
+     * Executes the specified reCAPTCHA action and retrieves the corresponding reCAPTCHA token.
+     *
+     * @param {RecaptchaAction} action - An instance of RecaptchaAction representing the action to be executed.
+     * @returns {Promise<Object>} A promise that resolves to an object containing:
+     *   - {string} token - The reCAPTCHA token.
+     *   - {Object} headerOptions - The header options for API requests including the reCAPTCHA token.
+     */
     const executeRecaptcha = async (action: RecaptchaAction) => {
-        /**
-         * Wait for the recaptchaInstance to be loaded
-         * by calling the recaptchaLoaded method.
-         * This ensures that the reCAPTCHA library is fully loaded
-         * and ready to execute reCAPTCHA actions.
-         */
+        // wait for the reCAPTCHA instance to be loaded.
         await recaptchaInstance?.recaptchaLoaded()
+
+        // execute the reCAPTCHA action and obtain the token.
         const token = await recaptchaInstance?.executeRecaptcha(action.name)
 
         const headerOptions = {
