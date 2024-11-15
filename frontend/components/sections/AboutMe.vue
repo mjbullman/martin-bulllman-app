34f7cce27705d37fda3921befd66f93925c2b7fe
Martin Bullman <martin.j.bullman@gmail.com>
2024-11-15 22:53:16 +0200

    Add about me section to About page

---
+++ frontend/components/sections/AboutMe.vue
@@ -0,0 +1,73 @@
+<template>
+
+    <v-row>
+
+        <v-col cols="12" sm="12" md="4" lg="5" xl="5" class="text-center">
+
+            <nuxt-img src="/img/personal/martin_hektor_container_hotel.webp" class="profile-image" alt="Martin at Hektor Container Hotel"></nuxt-img>
+
+        </v-col>
+
+        <v-col cols="12" sm="12" md="8" lg="7" xl="7" class="">
+
+            <h2 class="text-gradient text-h4 text-medium-emphasis mb-5 text-center">
+
+                Welcome to my digital corner!
+
+            </h2>
+
+            <h3 class="text-medium-emphasis mb-5 text-center">
+
+                I'm a Software Engineer crafting code and solutions from
+                {{ weather ? weather.current.temp_c : 0 }}&deg;C Tallinn, Estonia
+
+            </h3>
+
+            <div class="mb-5 text-justify">
+
+                My journey into technology began with curiosity and experimentation, evolving
+                into a deep fascination with software development's limitless potential. In
+                my early years, I immersed myself in coding, driven to understand how things
+                worked under the hood.
+
+            </div>
+
+            <div class="mb-5 text-justify">
+
+                Over time, I've mastered a diverse set of programming languages, frameworks,
+                and tools, enabling me to tackle projects across a wide spectrum of domains.
+                From designing efficient algorithms to architecting robust, scalable systems,
+                I've grown alongside the tech landscape.
+
+            </div>
+
+            <div class="text-justify">
+                Today, I focus on building efficient, user-centered solutions, always
+                seeking to optimize and innovate. Technology never stands still, and
+                neither do I.
+            </div>
+
+        </v-col>
+
+    </v-row>
+
+</template>
+
+<script setup lang="ts">
+
+    // composable
+    const runtimeConfig = useRuntimeConfig()
+
+    // api requests..
+    const { data: weather } = useFetch(runtimeConfig.public.apiBaseUrl + '/weather/current')
+
+</script>
+
+<style scoped>
+
+    .profile-image {
+        width: 300px;
+        border-radius: 15px 50px 30px;
+    }
+
+</style>
--- frontend/pages/About.vue
+++ frontend/pages/About.vue
@@ -6,44 +6,7 @@
 
         <v-container>
 
-            <v-row>
-
-                <v-col cols="12" sm="12" md="4" lg="5" xl="5" class="text-center">
-
-                    <nuxt-img src="/img/personal/martin_hektor_container_hotel.webp" width="300px" alt="Martin at Hektor Container Hotel"></nuxt-img>
-
-                </v-col>
-
-                <v-col cols="12" sm="12" md="8" lg="7" xl="7" class="text-center">
-
-                    <h2 class="text-h4 text-medium-emphasis mb-10">
-
-                        Welcome to my digital corner! I'm Martin, a Software Engineer crafting code and solutions
-                        from {{ weather ? weather.current.temp_c : 0 }}&deg;C Tallinn, Estonia
-
-                    </h2>
-
-                    <div class="mb-5">
-
-                        From a young age, I was captivated by the infinite possibilities that technology presented.
-                        What began as a curiosity quickly blossomed into a full-fledged passion as I delved deeper
-                        into the world of software development. Over the years, I've honed my skills, embracing
-                        new technologies and methodologies to stay at the forefront of this dynamic field.
-
-                    </div>
-
-                    <div>
-
-                        With a foundation built on a diverse array of programming languages and frameworks, I thrive
-                        on tackling complex problems head-on. Whether it's crafting elegant algorithms, architecting
-                        scalable systems, or optimizing user experiences, I approach each challenge with creativity,
-                        precision, and a relentless pursuit of excellence.
-
-                    </div>
-
-                </v-col>
-
-            </v-row>
+            <about-me></about-me>
 
             <!-- spotify -->
             <spotify></spotify>
@@ -56,12 +19,10 @@
 
 <script setup lang="ts">
 
+    // components.
+    import AboutMe     from '~/components/sections/AboutMe.vue'
     import Spotify     from '~/components/sections/Spotify.vue'
     import PageHeading from '~/components/headings/PageHeading.vue'
-
-    const runtimeConfig = useRuntimeConfig()
-
-    const { data: weather } = useFetch(runtimeConfig.public.apiBaseUrl + '/weather/current')
 
 </script>
 
