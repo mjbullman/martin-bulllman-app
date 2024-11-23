1b0c5a72898c2afc6028ac0c71a4e5a60c974342
Martin Bullman <martin.j.bullman@gmail.com>
2024-11-23 22:24:32 +0200

    Update core modules with commenting.

--- backend/martinbullman/core/admin.py
+++ backend/martinbullman/core/admin.py
@@ -1,3 +1,3 @@
-from django.contrib import admin
-
-# Register your models here.
+"""
+Admin configurations for the core module of Martin Bullman application.
+"""
--- backend/martinbullman/core/apps.py
+++ backend/martinbullman/core/apps.py
@@ -1,6 +1,12 @@
+"""
+Apps configurations for the core module of Martin Bullman application.
+"""
 from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
+    """
+    Configuration class for the Core app in the Martin Bullman application.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
