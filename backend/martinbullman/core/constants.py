f65fc83c50cfb70f5527169463d6833889cddd2f
Martin Bullman <martin.j.bullman@gmail.com>
2024-11-30 20:50:15 +0200

    Move email service to core app.

--- backend/martinbullman/core/constants.py
+++ backend/martinbullman/core/constants.py
@@ -1,3 +1,6 @@
 """ This module defines constants for Core application. """
 
+CONTACT_FORM_RECEIPT_TEMPLATE_ID = 6172693
+CONTACT_FORM_MESSAGE_TEMPLATE_ID = 6187155
+
 GOOGLE_RECAPTCHA_SITE_VERIFY = 'https://www.google.com/recaptcha/api/siteverify'
