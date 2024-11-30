cff5df5570c78bb2b289e1e854e972cd4d1d0319
Martin Bullman <martin.j.bullman@gmail.com>
2024-11-30 10:16:59 +0200

    Add api url to constants files.

--- backend/martinbullman/spotify/constants.py
+++ backend/martinbullman/spotify/constants.py
@@ -1,4 +1,7 @@
 """ This module defines constants for Spotify application. """
 
+from decouple import config
+
 SPOTIFY_API_URL = 'https://api.spotify.com/v1'
 SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token'
+SPOTIFY_APP_URL = f"{config('API_URL')}/spotify"
--- backend/martinbullman/weather/constants.py
+++ backend/martinbullman/weather/constants.py
@@ -1,3 +1,6 @@
 """ This module defines constants for Weather application. """
 
+from decouple import config
+
 WEATHER_API_URL = 'https://api.weatherapi.com/v1'
+WEATHER_APP_URL = f"{config('API_URL')}/weather"
