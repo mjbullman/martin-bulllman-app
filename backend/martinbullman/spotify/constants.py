405ec28d1ed19f4e29fb5804babb08df0afd1eaa
Martin Bullman <martin.j.bullman@gmail.com>
2024-11-30 10:18:26 +0200

    Update Spotify constants.

--- backend/martinbullman/spotify/constants.py
+++ backend/martinbullman/spotify/constants.py
@@ -2,6 +2,6 @@
 
 from decouple import config
 
+SPOTIFY_APP_URL = f"{config('API_URL')}/spotify"
 SPOTIFY_API_URL = 'https://api.spotify.com/v1'
 SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token'
-SPOTIFY_APP_URL = f"{config('API_URL')}/spotify"
