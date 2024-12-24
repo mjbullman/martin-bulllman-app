f190a11c615e9ddcc70959e0f89790fa4df7cdf2
Martin Bullman <martin.j.bullman@gmail.com>
2024-12-24 11:49:50 +0200

    Update plugins to conform to lint rules and styles.

--- frontend/plugins/vuetify.ts
+++ frontend/plugins/vuetify.ts
@@ -1,23 +1,23 @@
 // vuetify plugin.
 import { createVuetify } from 'vuetify'
-import { aliases, fa }   from 'vuetify/iconsets/fa-svg'
+import { aliases, fa } from 'vuetify/iconsets/fa-svg'
 
 // light theme configuration.
 const customLightTheme = {
     dark: false,
     colors: {
-        primary               : '#2196F3',
-        'primary-darken-1'    : '#0367B4',
-        secondary             : '#02E8CA',
-        'secondary-darken-1'  : '#00937F',
-        tertiary              : '#081AE0',
-        'tertiary-darken-1'   : '#050E82',
-        quaternary            : '#FB8C00',
-        'quaternary-darken-1' : '#9A5600',
-        error                 : '#F44336',
-        info                  : '#2196F3',
-        success               : '#4CAF50',
-        warning               : '#FF9800'
+        'primary': '#2196F3',
+        'primary-darken-1': '#0367B4',
+        'secondary': '#02E8CA',
+        'secondary-darken-1': '#00937F',
+        'tertiary': '#081AE0',
+        'tertiary-darken-1': '#050E82',
+        'quaternary': '#FB8C00',
+        'quaternary-darken-1': '#9A5600',
+        'error': '#F44336',
+        'info': '#2196F3',
+        'success': '#4CAF50',
+        'warning': '#FF9800'
     }
 }
 
@@ -25,25 +25,25 @@ const customLightTheme = {
 const customDarkTheme = {
     dark: true,
     colors: {
-        primary               : '#2196F3',
-        'primary-darken-1'    : '#0367B4',
-        secondary             : '#02E8CA',
-        'secondary-darken-1'  : '#00937F',
-        tertiary              : '#081AE0',
-        'tertiary-darken-1'   : '#050E82',
-        quaternary            : '#FB8C00',
-        'quaternary-darken-1' : '#9A5600',
-        error                 : '#F44336',
-        info                  : '#2196F3',
-        success               : '#4CAF50',
-        warning               : '#FF9800'
+        'primary': '#2196F3',
+        'primary-darken-1': '#0367B4',
+        'secondary': '#02E8CA',
+        'secondary-darken-1': '#00937F',
+        'tertiary': '#081AE0',
+        'tertiary-darken-1': '#050E82',
+        'quaternary': '#FB8C00',
+        'quaternary-darken-1': '#9A5600',
+        'error': '#F44336',
+        'info': '#2196F3',
+        'success': '#4CAF50',
+        'warning': '#FF9800'
     }
 }
 
 // vuetify nuxt plugin configuration.
-export default defineNuxtPlugin(nuxtApp => {
+export default defineNuxtPlugin((nuxtApp) => {
     // get the users dark mode pref from cookie.
-    const darkMode = useCookie('dark-mode')
+    // const darkMode = useCookie('dark-mode')
 
     const vuetify = createVuetify({
         ssr: true,
