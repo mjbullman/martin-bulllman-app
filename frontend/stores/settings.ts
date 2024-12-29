414d9eea6ca9789c5fc0979caab4908580e3849e
Martin Bullman <martin.j.bullman@gmail.com>
2024-12-29 13:01:29 +0200

    Update userSettings store to simplify logic.

--- frontend/stores/settings.ts
+++ frontend/stores/settings.ts
@@ -1,20 +1,9 @@
-import { useTheme } from 'vuetify'
 import { defineStore } from 'pinia'
-import { watch, ref, computed } from 'vue'
+import { ref, computed } from 'vue'
 
 export const useSettingsStore = defineStore('settings', () => {
-    const theme = useTheme()
-
     // state.
     const darkMode = ref<boolean>(true)
-    const cookie = useCookie<boolean>('dark-mode', {
-      default: () => darkMode.value,
-      watch: true
-    })
-
-    // sync darkMode with cookie on initialization.
-    darkMode.value = cookie.value ?? true
-    theme.global.name.value = darkMode.value ? 'customDarkTheme' : 'customLightTheme'
 
     // getters.
     const getDarkMode = computed(() => darkMode.value)
@@ -22,25 +11,11 @@ export const useSettingsStore = defineStore('settings', () => {
     // actions.
     function toggleDarkMode (): void {
       darkMode.value = !darkMode.value
-      cookie.value = darkMode.value
     }
-
-    // watchers.
-    watch(
-      darkMode,
-      (newValue) => {
-        theme.global.name.value = newValue ? 'customDarkTheme' : 'customLightTheme'
-      },
-      { immediate: true }
-    )
 
     return {
-      darkMode,
-      getDarkMode,
-      toggleDarkMode
+        darkMode,
+        getDarkMode,
+        toggleDarkMode
     }
-  },
-  {
-    persist: true // persist the pinia state.
-  }
-)
+})
