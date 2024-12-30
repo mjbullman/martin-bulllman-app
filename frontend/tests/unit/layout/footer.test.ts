e80bdd8cda582c979abf0477e1288257873b3f1e
Martin Bullman <martin.j.bullman@gmail.com>
2024-12-30 23:39:58 +0200

    Add unit tests for footer component.

---
+++ frontend/tests/unit/layout/footer.test.ts
@@ -0,0 +1,32 @@
+import { mountSuspended } from '@nuxt/test-utils/runtime'
+import type { DOMWrapper } from '@vue/test-utils'
+import FooterComponent from '~/components/layout/AppFooter.vue'
+
+describe('FooterComponent - User Interactions', () => {
+    let mountComponent: () => Promise<ReturnType<typeof mountSuspended>>
+
+    beforeEach(() => {
+        mountComponent = async () => mountSuspended(FooterComponent)
+    })
+
+    it('mounts successfully', async () => {
+        const component = await mountComponent()
+        expect(component.exists()).toBe(true)
+    })
+
+    it('external social links open in a new tab', async () => {
+        const component = await mountComponent()
+
+        const socialButtons = component.findAll('.v-btn')
+
+        socialButtons.forEach((link: DOMWrapper<Element>) => {
+            expect(link.attributes('target')).toBe('_blank')
+        })
+    })
+
+    it('displays the correct year in the footer', async () => {
+        const component = await mountComponent()
+        const year = new Date().getFullYear()
+        expect(component.text()).toContain(`Martin Bullman ${year} ©`)
+    })
+})
