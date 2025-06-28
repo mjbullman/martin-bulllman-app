440e2ec14a2dd19f486c9d89d656bff9b15b408a
Martin Bullman <martin.j.bullman@gmail.com>
2025-06-29 01:12:19 +0300

    Update plugins with better comments.

--- frontend/plugins/recaptcha.ts
+++ frontend/plugins/recaptcha.ts
@@ -1,12 +1,26 @@
+// plugins/recaptcha.ts
+
 import { VueReCaptcha } from 'vue-recaptcha-v3'
 import type { IReCaptchaOptions } from 'vue-recaptcha-v3/dist/IReCaptchaOptions'
 
 /**
- * The plugin enables the usage of Google reCAPTCHA in a Nuxt.js application.
+ * Nuxt 3 plugin to enable Google reCAPTCHA v3 globally.
+ *
+ * This plugin registers the `vue-recaptcha-v3` plugin with configuration
+ * using the public runtime config `recaptchaSiteKey`.
+ *
+ * You can then use `useRecaptcha()` in your components to trigger validation.
+ *
+ * Docs: https://www.npmjs.com/package/vue-recaptcha-v3.
  */
 export default defineNuxtPlugin((nuxtApp) => {
     const config = useRuntimeConfig()
 
+    /**
+     * reCAPTCHA options used by the plugin.
+     *
+     * @type {IReCaptchaOptions}
+     */
     const options: IReCaptchaOptions = {
         siteKey: config.public.recaptchaSiteKey,
         loaderOptions: {
